168777dd86dde9cf168ee79a8bfe471b34242a26
mariannacoppa <densimetra@gmail.com>
2024-07-10 16:57:31 +0200

    [UPD] initialised score and bombs arrays

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@
 const btn = document.getElementById('start');
 // recupero il tag che conterrà la griglia
 const grid = document.getElementById('grid');
+//definisco gli array contenenti il risultato e le bombe
+let scoreArray = [];
+let bombsArray = [];
