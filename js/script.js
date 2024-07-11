4a1568cd62e1fbf60c6638f5cb65c2deda6e9b84
mariannacoppa <densimetra@gmail.com>
2024-07-11 22:14:46 +0200

    [FIX] removed click after finding a bomb and show final score

--- js/script.js
+++ js/script.js
@@ -2,9 +2,33 @@
 const btn = document.getElementById('start');
 // recupero il tag che conterrà la griglia
 const grid = document.getElementById('grid');
-//definisco gli array contenenti il risultato e le bombe
+//dichiaro l'array del punteggio, delle bombe e la variabile dell'elemento selezionato
 let scoreArray = [];
 let bombsArray = [];
+let selectedItem = -1
+let listener = function (event) {
+    // console.log(event);
+    let selectedItem = parseInt(event.target.innerHTML);
+    scoreArray.push(scoreArray[selectedItem]);
+    console.log(selectedItem);
+            // se clicco una bomba
+            if (bombsArray.indexOf(selectedItem, 0) != -1) {
+                // la casella diventa rossa
+                this.classList.toggle('clicked-red');
+                // impedisce di cliccare altre caselle
+                removeAllClickListener();
+                alert(`Partita terminata! Hai totalizzato ${scoreArray.length - 1} punti`);
+            }
+            else {
+                // aggiungo/tolgo la classe clicked-blue e lo mostro in console
+                this.classList.toggle('clicked-blue');
+            }
+        }
+
+let options = {
+    once: true
+}
+
 // definisco la funzione che crea i quadrati della griglia
 function createSquare() {
     // creazione del tag html che diventerà il singolo quadrato della griglia
@@ -14,13 +38,15 @@ function createSquare() {
     // restituisco il quadrato contenuto nella variabile currentElement
     return currentElement;
 }
-// definisco la funzione che crea le bombe
 function createBombArray() {
     for (let i = 0; i < 16; i++) {
         // genero 16 numeri casuali
         let randomNumber = Math.floor((Math.random() * 100 + 1));
         bombsArray.push(randomNumber);
-        // impedisco la creazione di bombe con lo stesso numero
+    }
+}
+function removeDuplicates() {
+    for (let i = 0; i < bombsArray.length; i++) {
         for (let j = 0; j < bombsArray.length; j++) {
             if (bombsArray[i] == bombsArray[j] && i != j) {
                 bombsArray[i] = Math.floor((Math.random() * 100 + 1));
@@ -28,18 +54,18 @@ function createBombArray() {
         }
     }
 }
-// function removeDuplicates() {
-//     for (let i = 0; i < bombsArray.length; i++) {
-//         for (let j = 0; j < bombsArray.length; j++) {
-//             if (bombsArray[i] == bombsArray[j] && i != j) {
-//                 bombsArray[i] = Math.floor((Math.random() * 100 + 1));
-//             }
-//         }
-//     }
-// }
+function removeAllClickListener() {
+    let squareArray = document.body.getElementsByClassName("square")
+
+    for (let i = 0; i < squareArray.length; i ++) {
+        squareArray[i].removeEventListener('click', listener, false);
+    }
+}
+
 // chiamo le funzioni
 createBombArray();
-// removeDuplicates();
+removeDuplicates();
+
 // assegno l'evento click al pulsante recuperato nel dom
 btn.addEventListener('click', function () {
     // svuoto la griglia da tutti i suoi elementi interni (al click sl pulsante, non  si ripete la griglia)
@@ -49,22 +75,7 @@ btn.addEventListener('click', function () {
         // creo il singolo quadrato chiamando la funzione createSquare
         let currentSquare = createSquare();
         // aggiungo l'evento click al quadrato creato
-        let listener = function () {
-            console.log(i + 1);
-            // se clicco una bomba
-            if (bombsArray.indexOf(i, 0) != -1) {
-                // la casella diventa rossa
-                this.classList.toggle('clicked-red');
-                // impedisce di cliccare altre caselle
-                currentSquare.removeEventListener('click', listener);
-                // this.removeEventListener('click', this);
-            }
-            else {
-                // aggiungo/tolgo la classe clicked-blue e lo mostro in console
-                this.classList.toggle('clicked-blue');
-            }
-        }
-        currentSquare.addEventListener('click', listener);
+        currentSquare.addEventListener('click', listener, options);
         // assegno il numero progressivo
         currentSquare.innerText = i + 1;
         // appendo il quadrato all'interno della griglia
