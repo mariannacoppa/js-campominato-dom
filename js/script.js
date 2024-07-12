fca8d2f658c982469fc6382cddc04dad8630d0d7
mariannacoppa <densimetra@gmail.com>
2024-07-12 08:34:52 +0200

    [UPD] added win alert that appears when all squares without bombs are clicked FINE ESERCIZIO

--- js/script.js
+++ js/script.js
@@ -5,7 +5,46 @@ const grid = document.getElementById('grid');
 //dichiaro l'array del punteggio, delle bombe e la variabile dell'elemento selezionato
 let scoreArray = [];
 let bombsArray = [];
+let squareArray = [];
 let selectedItem = -1
+
+// definisco la funzione che crea i quadrati della griglia
+function createSquare() {
+    // creazione del tag html che diventerà il singolo quadrato della griglia
+    const currentElement = document.createElement('div');
+    // assegnazione della classe square all'elemento creato
+    currentElement.classList.add('square');
+    // restituisco il quadrato contenuto nella variabile currentElement
+    return currentElement;
+}
+// funzione che crea le bombe
+function createBombArray() {
+    for (let i = 0; i < 16; i++) {
+        // genero 16 numeri casuali
+        let randomNumber = Math.floor((Math.random() * 100 + 1));
+        bombsArray.push(randomNumber);
+        // impedisco la creazione di bombe con lo stesso numero
+        for (let j = 0; j < bombsArray.length; j++) {
+            if (bombsArray[i] == bombsArray[j] && i != j) {
+                bombsArray[i] = Math.floor((Math.random() * 100 + 1));
+            }
+        }
+        // console.log(bombsArray[i]);
+    }
+}
+
+// creo funzione per togliere i click a fine partita
+function removeAllClickListener() {
+    let squareArray = document.body.getElementsByClassName("square");
+    
+    for (let i = 0; i < squareArray.length; i ++) {
+        squareArray[i].removeEventListener('click', listener, false);
+    }
+};
+
+// chiamo la funzione
+createBombArray();
+
 let listener = function (event) {
     // console.log(event);
     let selectedItem = parseInt(event.target.innerHTML);
@@ -19,6 +58,10 @@ let listener = function (event) {
                 removeAllClickListener();
                 alert(`Partita terminata! Hai totalizzato ${scoreArray.length - 1} punti`);
             }
+            // se clicco tutte le caselle tranne quelle con la bomba
+            else if (scoreArray.length == squareArray.length - bombsArray.length) {
+                alert('Hai vinto!');
+            }
             else {
                 // aggiungo/tolgo la classe clicked-blue e lo mostro in console
                 this.classList.toggle('clicked-blue');
@@ -28,43 +71,6 @@ let listener = function (event) {
 let options = {
     once: true
 }
-
-// definisco la funzione che crea i quadrati della griglia
-function createSquare() {
-    // creazione del tag html che diventerà il singolo quadrato della griglia
-    const currentElement = document.createElement('div');
-    // assegnazione della classe square all'elemento creato
-    currentElement.classList.add('square');
-    // restituisco il quadrato contenuto nella variabile currentElement
-    return currentElement;
-}
-function createBombArray() {
-    for (let i = 0; i < 16; i++) {
-        // genero 16 numeri casuali
-        let randomNumber = Math.floor((Math.random() * 100 + 1));
-        bombsArray.push(randomNumber);
-    }
-}
-function removeDuplicates() {
-    for (let i = 0; i < bombsArray.length; i++) {
-        for (let j = 0; j < bombsArray.length; j++) {
-            if (bombsArray[i] == bombsArray[j] && i != j) {
-                bombsArray[i] = Math.floor((Math.random() * 100 + 1));
-            }
-        }
-    }
-}
-function removeAllClickListener() {
-    let squareArray = document.body.getElementsByClassName("square")
-
-    for (let i = 0; i < squareArray.length; i ++) {
-        squareArray[i].removeEventListener('click', listener, false);
-    }
-}
-
-// chiamo le funzioni
-createBombArray();
-removeDuplicates();
 
 // assegno l'evento click al pulsante recuperato nel dom
 btn.addEventListener('click', function () {
@@ -80,5 +86,7 @@ btn.addEventListener('click', function () {
         currentSquare.innerText = i + 1;
         // appendo il quadrato all'interno della griglia
         grid.append(currentSquare);
+        squareArray.push(currentSquare);
+        // console.log(squareArray[i]);
     }
 });
